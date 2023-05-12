a98606f623552a4a9747697cf7cf2d66574c6470
iswarning <iswarning@gmail.com>
2023-05-12 19:12:45 +0700

    update

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const server = https.createServer(app);
+const http = require('http');
+const server = http.createServer(app);
 const { ExpressPeerServer } = require("peer");
 
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
     }
 });
 
 const peerServer = ExpressPeerServer(server, {
-	debug: true,
-	path: "/myapp",
-  port: 443
+    debug: true,
+    path: "/myapp"
 });
 
 app.use("/peerjs", peerServer);
 
-peerServer.on('connection', (client) => { {
-  console.log(client)
-  console.log('connected')
-} });
+peerServer.on('connection', (client) => {
+    {
+        console.log(client)
+        console.log('connected')
+    }
+});
 
-https.get('/hello', () => {
-  return 'Hello';
-})
-// peerServer.on('disconnect', (client) => { ... });
+app.get('/hello', (req, res) => {
+        res.json('Hello');
+    })
+    // peerServer.on('disconnect', (client) => { ... });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+        io.emit('chat message', msg);
     });
     socket.on('join-room', (roomId, userId) => {
-      socket.join(roomId);
-      socket.to(roomId).emit('user-connected', userId);
-      socket.on('disconnect', () => {
-        socket.to(roomId).emit('user-disconnected', userId)
-      })
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
     })
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server running on port: ' + 3000)
+});
