a59dfe171ba8450e4d108ccd683aa57c4a473c19
iswarning <iswarning@gmail.com>
2023-05-12 19:44:27 +0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const { ExpressPeerServer } = require("peer");
+// const { ExpressPeerServer } = require("peer");
 const app = express();
 
-app.use(cors({
-    origin: true
-}));
+// app.use(cors({
+//     origin: true
+// }));
 
 app.get("/", (req, res, next) => res.send("Hello world!"));
 
-const server = app.listen(9000);
+app.listen(80);
 
-const peerServer = ExpressPeerServer(server, {
-    path: "/myapp",
-});
+// const peerServer = ExpressPeerServer(server, {
+//     path: "/myapp",
+// });
 
-app.use("/peerjs", peerServer);
+// app.use("/peerjs", peerServer);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
+// app.use((err, req, res, next) => {
+//     console.error(err.stack)
+//     res.status(500).send('Something broke!')
+// })
