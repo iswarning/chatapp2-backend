c4482c3330ecf9ce8c64e3cf60832f6699fdccc4
iswarning <iswarning@gmail.com>
2023-05-20 20:41:50 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-import cors from 'cors';
 import express from 'express';
 import { createServer } from 'http';
 import { PeerServer } from 'peer';
 import { Server } from 'socket.io';
 
 const app = express();
-app.use(cors())
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+});
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
