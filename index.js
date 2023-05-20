4fa68547bf1a951c683580ced17e30d7d2a38748
iswarning <iswarning@gmail.com>
2023-05-20 20:07:28 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import { createServer } from 'http';
 import { ExpressPeerServer } from 'peer';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-
-// const io = require('socket.io')(server, {
-//     cors: {
-//         origin: '*',
-//     }
-// });
+const io = new Server(server, { cors: { origin: '*' } });
 
 const peerServer = ExpressPeerServer(server, {
     proxied: true,
@@ -18,29 +14,29 @@ const peerServer = ExpressPeerServer(server, {
     ssl: {},
 });
 
-// app.use(peerServer);
+app.use(peerServer);
 
-// io.on('connection', socket => {
+io.on('connection', socket => {
 
-//     socket.on("send-message", (msg) => {
-//         io.emit("response-message", msg);
-//     })
+    socket.on("send-message", (msg) => {
+        io.emit("response-message", msg);
+    })
 
-//     socket.on('call-video', (senderCall, recipientCall, chatId) => {
-//         io.emit("response-call", senderCall, recipientCall, chatId)
-//     });
+    socket.on('call-video', (senderCall, recipientCall, chatId) => {
+        io.emit("response-call", senderCall, recipientCall, chatId)
+    });
 
-//     socket.on('reject-call', (data) => {
-//         io.emit("response-reject", data)
-//     });
+    socket.on('reject-call', (data) => {
+        io.emit("response-reject", data)
+    });
 
-//     socket.on('join-room', (roomId, userId, recipientId) => {
-//         socket.join(roomId)
-//         socket.to(roomId).emit('user-connected', userId)
-//         socket.on('disconnect', () => {
-//             socket.to(roomId).emit('user-disconnected', userId)
-//         })
-//     })
-// })
+    socket.on('join-room', (roomId, userId, recipientId) => {
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', userId)
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    })
+})
 
-peerServer.listen(9000, () => console.log('server peer running on port 9000'))
+server.listen(9000, () => console.log('server peer running on port 9000'))
