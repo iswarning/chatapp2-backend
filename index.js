abfb960221e1e9abc6296c6bdd368f42ade3c535
iswarning <iswarning@gmail.com>
2023-05-20 20:48:54 +0700

    update

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ import { PeerServer } from 'peer';
 import { Server } from 'socket.io';
 
 const app = express();
-app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-});
+
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
@@ -42,9 +31,9 @@ io.on('connection', socket => {
     })
 })
 
-PeerServer({
-    path: "/peer",
-    port: 443
-});
+// PeerServer({
+//     path: "/peer",
+//     port: 443
+// });
 
 server.listen(9000, () => console.log('server peer running on port 9000'))
