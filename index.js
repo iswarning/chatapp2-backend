647f6c0d5ff753fe4efb7a43f0b632005eb58c4b
iswarning <iswarning@gmail.com>
2023-05-20 19:25:42 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const serverPeer = http.Server(app);
+const server = http.Server(app);
 
-const ioPeer = require('socket.io')(serverPeer, {
+const io = require('socket.io')(server, {
     cors: {
         origin: '*',
     }
@@ -12,22 +12,21 @@ const ioPeer = require('socket.io')(serverPeer, {
 const PeerServer = require('peer').PeerServer;
 PeerServer({ port: 443, path: '/' });
 
-ioPeer.on('connection', socket => {
+io.on('connection', socket => {
 
-    socket.on("sendMessage", (msg) => {
-        ioPeer.emit("responseMessage", msg);
+    socket.on("send-message", (msg) => {
+        io.emit("response-message", msg);
     })
-
-    // socket.on("disconnect", () => {
-    //     console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
-    // });
 
     socket.on('call-video', (senderCall, recipientCall, chatId) => {
-        ioPeer.emit("response-call", senderCall, recipientCall, chatId)
-    })
+        io.emit("response-call", senderCall, recipientCall, chatId)
+    });
+
+    socket.on('reject-call', (data) => {
+        io.emit("response-reject", data)
+    });
 
     socket.on('join-room', (roomId, userId, recipientId) => {
-        console.log(roomId, userId, recipientId);
         socket.join(roomId)
         socket.to(roomId).emit('user-connected', userId)
         socket.on('disconnect', () => {
@@ -36,4 +35,4 @@ ioPeer.on('connection', socket => {
     })
 })
 
-serverPeer.listen(9000, () => console.log('server peer running on port 9000'))
+server.listen(9000, () => console.log('server peer running on port 9000'))
