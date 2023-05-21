bfce6b1b83718993e4ef690ba35b44ce28a5d265
iswarning <iswarning@gmail.com>
2023-05-21 18:51:59 +0700

    update

--- index.js
+++ index.js
@@ -2,33 +2,53 @@ import express from 'express';
 import { createServer } from 'http';
 import { PeerServer } from 'peer';
 import { Server } from 'socket.io';
+import cors from 'cors'
 
 const app = express();
 
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
+let userBusy = [];
+
 io.on('connection', socket => {
 
     socket.on("send-message", (msg) => {
         io.emit("response-message", msg);
-    })
+    });
 
-    socket.on('call-video', (senderCall, recipientCall, chatId) => {
-        io.emit("response-call", senderCall, recipientCall, chatId)
+    socket.on('call-video', (data) => {
+        let d = JSON.parse(data);
+        userBusy.push(d.sender);
+        if (typeof d.recipient === "string") {
+            userBusy.push(d.recipient);
+        } else {
+            d.recipient.forEach((re) => userBusy.push(re))
+        }
+        io.emit("response-call", data)
     });
 
     socket.on('reject-call', (data) => {
+        let d = JSON.parse(data);
+        userBusy = userBusy.filter((user) => user === d.sender && d.recipient.includes(user))
         io.emit("response-reject", data)
     });
 
-    socket.on('join-room', (roomId, userId, recipientId) => {
+    socket.on('join-room', (roomId, userId, recipient) => {
         socket.join(roomId)
         socket.to(roomId).emit('user-connected', userId)
         socket.on('disconnect', () => {
             socket.to(roomId).emit('user-disconnected', userId)
+            userBusy = [];
         })
     })
+
+})
+
+app.use(cors({ origin: '*' }));
+
+app.get("/getUserBusy", (req, res) => {
+    res.json(userBusy);
 })
 
 // PeerServer({
@@ -36,4 +56,4 @@ io.on('connection', socket => {
 //     port: 443
 // });
 
-server.listen(9000, () => console.log('server peer running on port 9000'))
+server.listen(9000, () => console.log('Server running on port 9000'))
